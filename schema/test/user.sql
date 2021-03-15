ec7bfc5a4b2b436738760fce3be2bba1b950827e
Alexey Vasyukov <a.vasyukov1@yandex.ru>
2021-03-15 23:22:48 +0300

    #5 Add test SQL script for users

---
+++ schema/test/user.sql
@@ -0,0 +1,4 @@
+INSERT INTO locations (latitude, longitude) VALUES (50, 87);
+
+INSERT INTO users (name, phone, password_hash, email, address_id)
+VALUES ('test', '78881007869', 'password', 'test@mail.ru', 1);
