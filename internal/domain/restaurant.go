76a49532cfe4ba7ff2cc4a5c7ba26d6bd14cb162
HammerFall42 <daniil.kolesnikoff@gmail.com>
2021-03-17 17:27:18 +0300

    #7 added restaurant domain

---
+++ internal/domain/restaurant.go
@@ -0,0 +1,12 @@
+package domain
+
+type Restaurant struct {
+	Id            int       `json:"id" db:"id"`
+	Name          string    `json:"name" db:"name"`
+	Phone         string    `json:"phone" db:"phone"`
+	Password      string    `json:"password" db:"password_hash"`
+	Email         string    `json:"email" db:"email"`
+	Address       *Location `json:"location" db:"location"`
+	WorkingStatus int       `json:"working_status" db:"working_status"`
+	Image		  string	`json:"image" db:"image"`
+}
