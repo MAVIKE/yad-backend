a7227e052d534e90bafc9c8134cbf29e1cd4a07e
EgorVolkov <volkovegor2000@yandex.ru>
2021-04-10 19:06:32 +0300

    #27 Add handler & swag info for deleting order

--- internal/delivery/http/v1/order.go
+++ internal/delivery/http/v1/order.go
@@ -15,6 +15,7 @@ func (h *Handler) initOrderRoutes(api *echo.Group) {
 		orders.Use(h.identity)
 		orders.POST("/", h.createOrder)
 		orders.GET("/:oid", h.getOrderById)
+		orders.DELETE("/:oid", h.deleteOrder)
 
 		orderItems := orders.Group("/:oid/items")
 		{
@@ -138,7 +139,7 @@ func (h *Handler) getOrderItems(ctx echo.Context) error {
 // @Failure 400,403,404 {object} response
 // @Failure 500 {object} response
 // @Failure default {object} response
-// @Router /orders/{oid}/ [get]
+// @Router /orders/{oid} [get]
 func (h *Handler) getOrderById(ctx echo.Context) error {
 	clientId, clientType, err := h.getClientParams(ctx)
 	if err != nil {
@@ -156,6 +157,38 @@ func (h *Handler) getOrderById(ctx echo.Context) error {
 	}
 
 	return ctx.JSON(http.StatusOK, orderItem)
+}
+
+// @Summary Delete Order
+// @Security UserAuth
+// @Tags orders
+// @Description delete order
+// @ModuleID deleteOrder
+// @Accept  json
+// @Produce  json
+// @Param oid path string true "Order id"
+// @Success 200 {object} response
+// @Failure 400,403,404 {object} response
+// @Failure 500 {object} response
+// @Failure default {object} response
+// @Router /orders/{oid} [delete]
+func (h *Handler) deleteOrder(ctx echo.Context) error {
+	_, _, err := h.getClientParams(ctx)
+	if err != nil {
+		return newResponse(ctx, http.StatusInternalServerError, err.Error())
+	}
+
+	orderId, err := strconv.Atoi(ctx.Param("oid"))
+	if err != nil || orderId == 0 {
+		return newResponse(ctx, http.StatusBadRequest, "Invalid orderId")
+	}
+
+	// err = h.services.Order.Delete(clientId, clientType, orderId, orderItemId)
+	// if err != nil {
+	// 	return newResponse(ctx, http.StatusInternalServerError, err.Error())
+	// }
+
+	return ctx.JSON(http.StatusOK, nil)
 }
 
 type orderItemInput struct {
